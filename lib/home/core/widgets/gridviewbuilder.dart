ca9ac783f58ed2bb44d481c89842a279a2a9a930
yakubovdilshod481-create <yakubovdilshod481@gmail.com>
2026-05-11 00:49:04 +0500

    new commit

--- lib/home/core/widgets/gridviewbuilder.dart
+++ lib/home/core/widgets/gridviewbuilder.dart
@@ -3,6 +3,7 @@ import 'package:clot_project/home/presentation/model/categories_model/hoodies_in
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+// ignore: must_be_immutable
 class Gridviewbuilder extends StatelessWidget {
   List<HoodiesInCategories> products = [
     HoodiesInCategories(
